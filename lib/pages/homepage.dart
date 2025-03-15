b4937c74b6dcc82f8c0c8afa629c3c8e731b152d
Mark Dhave O. Repala <repala@Marks-MacBook-Air.local>
2025-03-15 12:05:25 +0800

    fix

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,48 +6,160 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
         toolbarHeight: 100,
-        
-        backgroundColor: const Color.fromARGB(255, 236, 235, 240),
-        flexibleSpace: Padding( 
+        backgroundColor: const Color.fromARGB(255, 243, 243, 250),
+        flexibleSpace: Padding(
           padding: EdgeInsets.only(bottom: 10),
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          
-          children: [
-        Row(
-         
-          children: [
-          IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-          Container(
-            margin: EdgeInsets.only(left: 95),
-            child:
-          Text('Homepage', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                children: [
+                  IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+                  Container(
+                    margin: EdgeInsets.only(left: 95),
+                    child: Text('Homepage', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("All"), 
+                  Text("New"),
+                  Text("Saved"),
+                ],
+              ),
+            ],
           ),
-        ]),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-          Text("All"),
-          Text("New"),
-          Text("Saved"),
-         
-        ]),
-      ]),
-      
-       ) ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [ 
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children:[ 
+          
+
+          //HOME CONTAINER STARTS HERE
           Container(
             margin: EdgeInsets.only(top: 20),
-            child: Text('Homepage', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+            height: 200,
+            width: 350,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: AssetImage('assets/image.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.transparent, Colors.black.withOpacity(0.6)],
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(child: 
+                Icon(Icons.bookmark_outline, color: Colors.white, size: 30.0,),
+                top: 10,
+                left: 10,
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 15,
+                  child: Text('Home',
+                   style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),)),
+              
+              ],
+            ),
+            ),
+            //HOME CONTAINER ENDS HERE
+
+
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            height: 200,
+            width: 350,
+            decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: AssetImage('assets/suit.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned.fill(child: 
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+                    ),
+                  ),
+                ),
+                ),
+                Positioned(child: 
+                Icon(Icons.bookmark_outline, color: Colors.white, size: 30.0,),
+                top: 10,
+                left: 10,
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 10,
+                  child: Text('Self',
+                   style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),)),
+              ],
+            ),
+          ),Container(
+            
+            margin: EdgeInsets.only(top: 20),
+            height: 200,
+            width: 350,
+            decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: AssetImage('assets/image.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(child: 
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+                    )
+                  ),
+                )
+                ),
+                Positioned(child:
+                Icon(Icons.bookmark_outline, color: Colors.white, size: 30.0,),
+                top: 10,
+                left: 10,
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 10,
+                  child: Text('Others...',
+                   style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),)),
+              ],
+            ),
           ),
-          Text('Homeeepage')
-          
-          ]
-          ),
+  ]),
+      ),
     );
   }
 }
